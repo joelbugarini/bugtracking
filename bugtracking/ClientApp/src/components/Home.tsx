78676087b24534225062281a93e6fcfa9c7dd2f3
Joel Bugarini <joelbugarini@joelbugarini.local>
2019-11-01 13:49:29 -0700

    Deploying from git with mac

--- bugtracking/ClientApp/src/components/Home.tsx
+++ bugtracking/ClientApp/src/components/Home.tsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 const Home = () => (
   <div>
     <h1>Hello, world!</h1>
-    <p>Welcome to your new single-page application, built with:</p>
+    <p>Welcome to your new single-page application, built from macOS with:</p>
     <ul>
       <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
       <li><a href='https://facebook.github.io/react/'>React</a> and <a href='https://redux.js.org/'>Redux</a> for client-side code</li>
